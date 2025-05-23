b0b77f25b561ba955003ed20727388d600fb29ed
Lilith-Milante <anestshchuch@gmail.com>
2025-05-23 22:41:31 +0300

    code 2

--- http_service_py/main.py
+++ http_service_py/main.py
@@ -1,27 +1,29 @@
 from flask import Flask, request, jsonify
 import subprocess
 
-app = Flask(__name___)
-
 PORT = 3000
+
+app = Flask(__name__)
 
 @app.route("/query", methods=["POST"])
 def query_csv():
     sql = request.json.get("sql")
     if not sql:
-        return {"error": "SQL query missing"}, 400
+        return jsonify({"error": "Missing SQL query"}), 400
 
     try:
-        # Выполняем команду q через subprocess
+        # Запуск утилиты q
         result = subprocess.run(
             ["q", "-H", "--csv", sql],
-            cwd="/app",  # где лежит data.csv
+            cwd="/app",
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
+
         if result.returncode != 0:
-            return {"error": result.stderr.strip()}, 400
-        return {"result": result.stdout.strip()}
+            return jsonify({"error": result.stderr.strip()}), 400
+
+        return jsonify({"result": result.stdout.strip()})
     except Exception as e:
-        return {"error": str(e)}, 500
+        return jsonify({"error": str(e)}), 500
